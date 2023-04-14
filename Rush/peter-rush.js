e125f28af030dccd6ef43fccc2e2a350dc565cef
TheTrafficLight <109645458+TheTrafficLight@users.noreply.github.com>
2023-04-14 15:23:59 +0930

    Bug fixes
    Added bug fixes to parts of the landing page and my rush page

--- Rush/peter-rush.js
+++ Rush/peter-rush.js
@@ -1,5 +1,9 @@
 var background = document.getElementById("background");
 console.log(background);
 
-background.animate({ transform: "rotate(360deg)" },
-40000, Infinity);
+
+background.animate(
+    {transform: "rotate(360deg)"}, 40000
+)
+
+
